642aa6b01481d19cc29614f2ea5203f00715479f
NamNH <NamNH@mirabo.com.vn>
2022-04-26 10:09:34 +0700

    add router for model 3d

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import './App.css';
-import Scene from './Scene'
-import Office360 from './Office360'
-import Model3D from './Model3D'
+import "./App.css";
+import Scene from "./Scene";
+import Office360 from "./Office360";
+import Model3D from "./Model3D";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      {/* <Scene></Scene> */}
-      <Office360></Office360>
-      {/* <Model3D></Model3D> */}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Office360 />} />
+        <Route path="/model3d" element={<Model3D />} />
+      </Routes>
+    </BrowserRouter>
+
+    // <div className="App">
+    //   <Scene></Scene>
+    //   <Office360></Office360>
+    //   <Model3D></Model3D>
+    // </div>
   );
 }
 
